05556db31ecc98db55fc36f84b51f76157f212f5
iluvatar1 <woquendo@gmail.com>
2024-11-14 17:15:43 -0500

    Improved and fixed prime search function

--- 01-IntroCPP/codes-2024-11-13-workshop/prime.cpp
+++ 01-IntroCPP/codes-2024-11-13-workshop/prime.cpp
@@ -6,12 +6,14 @@ bool isprime(long num);
 
 int main(void)
 {
-    // // test cases
-    // std::cout << isprime(2) << "\n";
-    // std::cout << isprime(4) << "\n";
-    // std::cout << isprime(97) << "\n";
-    //std::cout << isprime(19999999967) << "\n";
-    std::cout << isprime( 200000000041) << "\n";
+    // test cases
+    std::cout << isprime(2) << "\n";
+    std::cout << isprime(4) << "\n";
+    std::cout << isprime(9) << "\n";
+    std::cout << isprime(27) << "\n";
+    std::cout << isprime(97) << "\n";
+    std::cout << isprime(19999999967) << "\n";
+    std::cout << isprime(200000000041) << "\n";
 
     /*
     for (int ii = 2; ii <= 150; ii = ii +1){
@@ -30,9 +32,12 @@ bool isprime(long num)
     if (num <= 1) {
         return false;
     }
+    if (num > 2 and num%2==0) {
+      return false;
+    }
     // buscar si tiene algun divisor
     //for (long ii = 2; ii <= num - 1; ii = ii + 1) // very slow
-    for (long ii = 2; ii <= std::sqrt(num)+1; ii = ii + 1) // very fast
+    for (long ii = 3; ii <= std::sqrt(num)+1; ii = ii + 2) // very fast
     {
         if (num%ii == 0) {
             return false;
