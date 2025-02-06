7f8b0d65a496c24442c55eb113a547fdee1ffa29
iluvatar1 <woquendo@gmail.com>
2025-02-06 16:54:52 -0500

    Added rk4 for harmonic oscillator

---
+++ 08-ODE-IVP/codes-2025-02-05/ode_rk4_harmonic.cpp
@@ -0,0 +1,84 @@
+#include <valarray>
+#include <iostream>
+
+// parameters
+const double T0 = 0.0;
+const double TF = 100.0;
+const double DT = 0.1;
+const double G = 9.81;
+const double B = 0.0;
+const double W = 2.3456;
+
+// state data struct
+using state_t = std::valarray<double>;
+
+// function declarations
+void print_system(double t, const state_t & s);
+void euler(double t, state_t & s);
+void rk4(double t, state_t & s);
+void deriv(double t, const state_t & s, state_t & dsdt);
+
+int main(int argc, char **argv)
+{
+    // state vector
+    state_t s(2);
+
+    // initial conditions
+    s[0] = 2.0; 
+    s[1] = 10.0;
+    print_system(0.0, s);
+
+    // time evolution
+    for (double t = T0; t <= TF; t += DT) {
+        // do one euler step
+        //euler(t, s);
+        rk4(t, s);
+        // print
+        print_system(t+DT, s);
+    }
+
+    return 0;
+}
+
+void print_system(double t, const state_t & s)
+{
+    std::cout << t << "\t" << s[0] << "\t" << s[1] << "\n";
+}
+
+void deriv(double t, const state_t & s, state_t & dsdt)
+{
+    dsdt[0] = s[1];
+    //dsdt[1] = -G - B*s[1];
+    dsdt[1] = -W*W*s[0];
+}
+
+void euler(double t, state_t & s)
+{
+    state_t dsdt(s.size());
+    deriv(t, s, dsdt);
+    // asumimos valarray
+    s = s + DT*dsdt;
+}
+
+void rk4(double t, state_t & s)
+{
+    // k1
+    state_t k1(s.size());
+    deriv(t, s, k1);
+
+    // k2
+    state_t k2(s.size());
+    deriv(t+DT/2, s + DT*k1/2, k2);
+
+    // k3
+    state_t k3(s.size());
+    deriv(t+DT/2, s + DT*k2/2, k3);
+
+    // k4
+    state_t k4(s.size());
+    deriv(t+DT, s + DT*k3, k4);
+
+    // t -> t+dt
+    s = s + DT*(k1 + 2*k2 + 2*k3 + k4)/6.0;
+}
+
